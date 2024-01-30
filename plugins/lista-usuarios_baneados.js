31227a6696f07c3f4f4d2c0daf1c426840cc0a50
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-01-29 19:56:29 -0500

    HamilyBot

--- plugins/lista-usuarios_baneados.js
+++ plugins/lista-usuarios_baneados.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, isOwner }) => {
 let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
 let caption = `
-*╭•·–| 👥 U̷S̷U̷A̷R̷I̷O̷S̷ ̷B̷A̷N̷E̷A̷D̷O̷S̷ ̷:̷ ̷B̷A̷N̷N̷E̷D̷ ̷U̷S̷E̷R̷S̷ |–·•*
+*╭•·–|𖣐 U̷S̷U̷A̷R̷I̷O̷S̷ ̷B̷A̷N̷E̷A̷D̷O̷S̷ ̷:̷ ̷B̷A̷N̷N̷E̷D̷ ̷U̷S̷E̷R̷S̷  𖣐|–·•*
 │ *Total : ${users.length} Usuarios* ${users ? '\n' + users.map(([jid], i) => `
 │
 │ *${i + 1}.* ${conn.getName(jid) == undefined ? 'Sin Usuarios Baneados en mi Lista' : conn.getName(jid)}
