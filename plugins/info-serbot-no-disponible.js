93e6aa77757620dc4a72910be7aa663dc7b96cde
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-02-01 07:55:32 -0500

    HamilyBot

--- plugins/info-serbot-no-disponible.js
+++ plugins/info-serbot-no-disponible.js
@@ -8,13 +8,13 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //} catch (e) {
 
 //} finally { }
-  await m.reply(`╰⊱⚠️⊱ *𝘼𝘿𝙑𝙀𝙍𝙏𝙀𝙉𝘾𝙄𝘼 | 𝙒𝘼𝙍𝙉𝙄𝙉𝙂* ⊱⚠️⊱╮`)
+  await m.reply(`╰⊱⚠️⊱ *A̷D̷V̷E̷R̷T̷E̷N̷C̷I̷A̷ | W̷A̷R̷N̷I̷N̷G̷* ⊱⚠️⊱╮`)
   //conn.sendButton(m.chat, str, author, await(await fetch(pp)).buffer(), [['Menu', '/menu']], m)
 let text = `
 *Las funciones | The functions #jadibot #bots #serbot #subbot #getcode. No estan disponibles en | Are not available in* ${gt}`.trim()   
 conn.reply(m.chat, text, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
+title: '𝗛𝗮𝗺𝗶𝗹𝘆𝗕𝗼𝘁 | 𒅒𝗕𝗿𝗮𝘀𝗵𝗸𝗶𝗲 𝗗𝗶𝗼𝘀𒅒',
 body: 'Super Bot WhatsApp',         
 previewType: 1, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
 sourceUrl: `${yt}`}}})  
@@ -23,12 +23,12 @@ sourceUrl: `${yt}`}}})
   `.trim()
   
   await conn.sendHydrated(m.chat, info, wm, null, ig, '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
-['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '/cuentasgb'],
-['𝙂𝙧𝙪𝙥𝙤𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝙂𝙧𝙤𝙪𝙥𝙨 🔰', '.grupos'],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+['𝒞𝓊𝑒𝓃𝓉𝒶𝓈 𝒪𝒻𝒾𝒸𝒾𝒶𝓁𝑒𝓈 | 𝒜𝒸𝒸𝑜𝓊𝓃𝓉𝓈 ✔', '/cuentasgb'],
+['𝒢𝓇𝓊𝓅𝑜𝓈 𝑜𝒻𝒾𝒸𝒾𝒶𝓁𝑒𝓈 | 𝒢𝓇𝑜𝓊𝓅𝓈 ✔', '.grupos'],
+['𝗩𝗼𝗹𝘃𝗲𝗿 𝗮𝗹 𝗺𝗲𝗻𝘂 | 𝗕𝗮𝗰𝗸 𝘁𝗼 𝗺𝗲𝗻𝘂 ✔', '/menu']
 ], m,)
   
-  //await conn.sendButton(m.chat, `Infórmate sobre las Novedades y recuerda tener la última versión.\nFind out about what's new and remember to have the latest version.`, author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', /${command}], ['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 2 🔄', /${command}]], m)
+  //await conn.sendButton(m.chat, `Infórmate sobre las Novedades y recuerda tener la última versión.\nFind out about what's new and remember to have the latest version.`, author, url, [['𖤗 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 𖤗', /${command}], ['𖤗 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 2 𖤗', /${command}]], m)
 }
 handler.command = /^(yafunka:3)/i
 export default handler
