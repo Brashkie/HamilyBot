12e9a5db00244fb2b73e542ee2f6780ce6eb721a
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-02-24 15:04:54 -0500

    HamilyBot

--- plugins/herramientas-zona_horaria.js
+++ plugins/herramientas-zona_horaria.js
@@ -22,27 +22,27 @@ const fechaasi = moment().tz('Asia/Jakarta').format('DD/MM HH:mm')
 const fechabra = moment().tz('America/Sao_Paulo').format('DD/MM HH:mm')
 const fechaafri = moment().tz('Africa/Malabo').format('DD/MM HH:mm')
 await conn.sendMessage(m.chat, { text: `\`\`\`
-「 ZONA-HORARIA 🗺️ 」
-⏱️Peru       : ${fechaper}
-⏱️Mexico     : ${fechamex}
-⏱️Bolivia    : ${fechabol}
-⏱️Chile      : ${fechachi}
-⏱️Argentina  : ${fechaarg}
-⏱️Colombia   : ${fechacol}
-⏱️Ecuador    : ${fechaecu}
-⏱️Costa_Rica : ${fechacosr}
-⏱️Cuba       : ${fechacub}
-⏱️Guatemala  : ${fechagua}
-⏱️Honduras   : ${fechahon}
-⏱️Nicaragua  : ${fechanic}
-⏱️Panama     : ${fechapan}
-⏱️Uruguay    : ${fechauru}
-⏱️Venezuela  : ${fechaven}
-⏱️Paraguay   : ${fechapar}
-⏱️New York   : ${fechanew}
-⏱️Indonesia  : ${fechaasi}
-⏱️Brasil     : ${fechabra}
-⏱️G.N.Q      : ${fechaafri}
+「 ░Z░O░N░A░-░H░O░R░A░R░I░A░ 」
+⌚Peru       : ${fechaper}
+⌚Mexico     : ${fechamex}
+⌚Bolivia    : ${fechabol}
+⌚Chile      : ${fechachi}
+⌚Argentina  : ${fechaarg}
+⌚Colombia   : ${fechacol}
+⌚Ecuador    : ${fechaecu}
+⌚Costa_Rica : ${fechacosr}
+⌚Cuba       : ${fechacub}
+⌚Guatemala  : ${fechagua}
+⌚Honduras   : ${fechahon}
+⌚Nicaragua  : ${fechanic}
+⌚Panama     : ${fechapan}
+⌚Uruguay    : ${fechauru}
+⌚Venezuela  : ${fechaven}
+⌚Paraguay   : ${fechapar}
+⌚New York   : ${fechanew}
+⌚Indonesia  : ${fechaasi}
+⌚Brasil     : ${fechabra}
+⌚G.N.Q      : ${fechaafri}
 \`\`\`
 ${String.fromCharCode(8206).repeat(850)}
 💻 *Zona horaria del servidor actual:*\n*[ ${Intl.DateTimeFormat().resolvedOptions().timeZone} ]*\n*${moment().tz(Intl.DateTimeFormat().resolvedOptions().timeZone).format('DD/MM/YY HH:mm:ss')}*` }, {quoted: m })
