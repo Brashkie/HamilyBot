bc50a6308fdd865e6a46b73f9e3e9c3970169128
Brashkie <142468732+Brashkie@users.noreply.github.com>
2024-03-03 17:03:52 -0500

    HamilyBot

--- plugins/sticker-pat.js
+++ plugins/sticker-pat.js
@@ -2,14 +2,14 @@ import { sticker } from '../lib/sticker.js'
 import fetch from 'node-fetch'
 let MessageType = (await import(global.baileys)).default
 let handler = async (m, { conn}) => {
-try {
+try {   
 if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
 if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
-let res = await fetch('https://api.waifu.pics/sfw/pat')
+let res = await fetch('https://nekos.life/api/kiss')
 let json = await res.json()
 let { url } = json
-let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} le dio palmaditas a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
+let stiker = await sticker(null, url, `+${m.sender.split('@')[0]} está besando a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`)
 conn.sendFile(m.chat, stiker, null, { asSticker: true })
 } catch (e) { }}
-handler.command = /^(palmaditas|cariños|mimos)$/i 
+handler.command = /^(besos|beso|besar|besando)$/i
 export default handler
